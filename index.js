acb4d53865bc1e572292faa1f6a27d76aebc0e4b
Blake Alexander <blake.alexander.13@gmail.com>
2018-09-14 11:53:15 -0700

    first attempt at scroll-then-fix

--- index.js
+++ index.js
@@ -127,5 +127,21 @@ document.getElementById('searchButton').addEventListener('click',  function (e) 
   handleSubmit();
 });
 
+function sticktothetop() {
+  var window_top = $(window).scrollTop();
+  var top = $('#stick-here').offset().top;
+  if (window_top > top) {
+      $('#stickThis').addClass('stick');
+      $('#stick-here').height($('#stickThis').outerHeight());
+  } else {
+      $('#stickThis').removeClass('stick');
+      $('#stick-here').height(0);
+  }
+}
+
+$(function() {
+  $(window).scroll(sticktothetop);
+  sticktothetop();
+});
 
 //give credit to newsapi.org
