b9ec1fe53185547b4d7423c84b6c6933f3413377
Blake Alexander <blake.alexander.13@gmail.com>
2020-06-08 14:05:06 -0700

    updated to use bing instead of newsapi

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ document.addEventListener("touchstart", function() {}, true);
 
 
 function getWiki(searchQuery, callback) {
-  let wikiEP = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${searchQuery}&limit=1&format=json&callback=?`;
+  // let wikiEP = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${searchQuery}&limit=1&format=json&callback=?`;
+  let wikiEP = `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=${searchQuery}`;
   $.ajax({
     type: 'GET',
     url: wikiEP,
@@ -17,6 +18,7 @@ function getWiki(searchQuery, callback) {
 
 //searches description given from API for Wiki's default phrase "may refer to" and replaces it with a more helpful explanation
 function filterWikiResults(wikiData) {
+  console.log(wikiData)
   if (wikiData[2][0].includes("may refer to")) {
     $('.historyContainer').append(`<p>Uh oh!  There seems to be a few places with that name.</p>
     <p>You can try making your search more specific, or clicking <a href="${wikiData[3][0]}" target="_blank">here</a> to see results on Wikipedia.</p>`);
@@ -40,21 +42,39 @@ function printWikiResults(wikiData) {
 }
 
 function getNews(searchQuery, callback) {
-  let newsEP = `https://newsapi.org/v2/everything?q=${searchQuery}&sortBy=relevancy&pageSize=5&page=${newsPageNumber}&apiKey=0b1e1594e959400e9c6179dd16bd5369`;
-  $.ajax({
-    type: 'GET',
-    url: newsEP,
-    contenttype: 'application/x-www-form-urlencoded; charset=UTF-8',
-    async: true,
-    datatype: 'jsonp',
-    success: callback,
+  let newsEP = `https://microsoft-azure-bing-news-search-v1.p.rapidapi.com/search?count=10&q=${searchQuery}`;
+  let settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": newsEP,
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "microsoft-azure-bing-news-search-v1.p.rapidapi.com",
+      "x-rapidapi-key": "a2a1d6b4bemshe811d0c37dc6c6dp1397d1jsn9c0c4b73a639"
+    }
+  }
+  
+  $.ajax(settings).done(function (response) {
+    callback(response)
+    console.log(response);
   });
 }
 
+// function getNews(searchQuery, callback) {
+//   let newsEP = `https://newsapi.org/v2/everything?q=${searchQuery}&sortBy=relevancy&pageSize=5&page=${newsPageNumber}&apiKey=0b1e1594e959400e9c6179dd16bd5369`;
+//   $.ajax({
+//     type: 'GET',
+//     url: newsEP,
+//     contenttype: 'application/x-www-form-urlencoded; charset=UTF-8',
+//     async: true,
+//     datatype: 'jsonp',
+//     success: callback,
+//   });
+// }
+
 //checks to see if there is more than one result and prints a response if there is not
 function filterNewsResults(newsData) {
-  console.log(newsData)
-  if (newsData.articles.length < 1) {
+  if (newsData.value.length < 1) {
     $('.newsContainer').html("<h2>Sorry!  We couldn't find any news results for that search!</h2>");
   }
   else {
@@ -64,12 +84,11 @@ function filterNewsResults(newsData) {
 
 function printNewsResults(newsData) {
   $('.newsContainer').prepend(`<h1 class="trendingNews">Trending News:</h1>`);
-  for (var i=0; i<newsData.articles.length; i++) { 
-    let title = newsData.articles[i].title;
-    let sourceName = newsData.articles[i].source.name;
-    let description = newsData.articles[i].description;
-    let articleUrl = newsData.articles[i].url;
-    console.log(title, sourceName, description)
+  for (var i=0; i<newsData.value.length; i++) { 
+    let title = newsData.value[i].name;
+    let sourceName = newsData.value[i].provider[0].name;
+    let description = newsData.value[i].description;
+    let articleUrl = newsData.value[i].url;
 
     $('.newsContainer').append(`<h2 class="newsArticleTitle">${title}</h2>
     <h3 class="newsContent">${sourceName}</h3>
